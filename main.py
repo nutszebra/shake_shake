e6b52bd74df3b7cf4164eb1b4c95eb79af52efbd
nutszebra <nutszebra@gmail.com>
2017-03-11 16:43:14 +0900

    fixed main script for cosine annealing

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
                         default='./',
                         help='model and optimizer will be saved every epoch')
     parser.add_argument('--epoch', '-e', type=int,
-                        default=300,
+                        default=1800,
                         help='maximum epoch')
     parser.add_argument('--batch', '-b', type=int,
                         default=128,
@@ -34,27 +34,31 @@ if __name__ == '__main__':
                         default=1,
                         help='start from this epoch')
     parser.add_argument('--train_batch_divide', '-trb', type=int,
-                        default=2,
+                        default=1,
                         help='divid batch number by this')
     parser.add_argument('--test_batch_divide', '-teb', type=int,
-                        default=2,
+                        default=1,
                         help='divid batch number by this')
-    parser.add_argument('--lr', '-lr', type=float,
+    parser.add_argument('--eta_max', '-ema', type=float,
                         default=0.2,
-                        help='leraning rate')
+                        help='eta max for cosine annealing')
+    parser.add_argument('--eta_min', '-emi', type=float,
+                        default=0.002,
+                        help='eta min for cosine annealing')
     parser.add_argument('--dim', '-dim', type=int,
                         default=64,
                         help='width')
 
     args = parser.parse_args().__dict__
-    lr = args.pop('lr')
+    eta_max = args.pop('eta_max')
+    eta_min = args.pop('eta_min')
     dim = args.pop('dim')
 
     print('generating model')
     model = shake_shake.ShakeShake(10, (dim, dim * 2, dim * 4), (4, 4, 4))
     print('Done')
     print('parameters: {}'.format(model.count_parameters()))
-    optimizer = nutszebra_optimizer.OptimizerPyramidalResNet(model, lr=lr)
+    optimizer = nutszebra_optimizer.OptimizerCosineAnnealing(model, eta_max=eta_max, eta_min=eta_min, total_epoch=args['epoch'])
     args['model'] = model
     args['optimizer'] = optimizer
     args['da'] = nutszebra_data_augmentation.DataAugmentationCifar10NormalizeSmall
